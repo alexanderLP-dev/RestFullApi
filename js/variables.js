5edc5fa6cfc68eacdfcb1eb220e0ea1139b7f69b
alexanderLP-dev <golovninalexandr87@gmail.com>
2019-12-17 16:27:01 +0200

    main

--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,4 @@
-export let studentsListElement = document.querySelector('#students-list');
-export let updateStudentFormElement = document.querySelector('#update-student-form');
+export let studentsListElement = document.querySelector("#students-list");
+export let updateStudentFormElement = document.querySelector(
+  "#update-student-form"
+); //
